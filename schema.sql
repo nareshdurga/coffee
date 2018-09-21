1240530dfa35e7afdae410a179cce9d93bbc74dc
NareshD <nareshblr2015@gmail.com>
2018-09-21 20:42:54 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,37 +1,40 @@
-CREATE TABLE `world`.`customer` (
-  `customer_id` INT NOT NULL,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `customer_type` VARCHAR(45) NULL,
-  `full_name` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `mobile` VARCHAR(45) NULL,
-  PRIMARY KEY (`customer_id`));
-  
-  CREATE TABLE `world`.`beverage` (
-  `beverage_id` INT NOT NULL,
-  `beverage_name` VARCHAR(45) NULL,
-  `beverage_cost` VARCHAR(45) NULL,
-  `beverage_type` VARCHAR(45) NULL,
-  PRIMARY KEY (`beverage_id`));
+CREATE TABLE `beverage` (
+  `idbeverage` int(11) NOT NULL,
+  `beverage_name` varchar(45) DEFAULT NULL,
+  `beverage_cost` varchar(45) DEFAULT NULL,
+  `beverage_status` varchar(45) DEFAULT NULL,
+  `beverage_created` varchar(45) DEFAULT NULL,
+  `sold_date` datetime DEFAULT NULL,
+  `beverage_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idbeverage`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-  
-  CREATE TABLE `world`.`order` (
-  `order_id` INT NOT NULL,
-  `customer_id` INT NULL,
-  `beverage_id` INT NULL,
-  `total_cost` DECIMAL NULL,
-  PRIMARY KEY (`order_id`),
-  INDEX `customer_id_idx` (`customer_id` ASC),
-  INDEX `beverage_id_idx` (`beverage_id` ASC),
-  CONSTRAINT `customer_id`
-    FOREIGN KEY (`customer_id`)
-    REFERENCES `world`.`customer` (`customer_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `beverage_id`
-    FOREIGN KEY (`beverage_id`)
-    REFERENCES `world`.`beverage` (`beverage_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `customer_type` varchar(45) DEFAULT NULL,
+  `full_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `mobile` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `order` (
+  `order_date` datetime DEFAULT NULL,
+  `total_amount` decimal(10,0) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `beverage_id` int(11) DEFAULT NULL,
+  `order_id` int(11) NOT NULL,
+  `order_status` varchar(255) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `order_ref` varchar(45) DEFAULT NULL,
+  KEY `customer_id_idx` (`customer_id`),
+  KEY `beverage_id_idx` (`beverage_id`),
+  CONSTRAINT `beverage_id` FOREIGN KEY (`beverage_id`) REFERENCES `beverage` (`idbeverage`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
